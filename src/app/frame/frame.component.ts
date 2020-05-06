53a3ad69e4167cd7735c21b8d0e3bb3ea37c52b5
DarVoid <jorgemduque@hotmail.com>
2020-05-06 23:10:19 +0100

    firebase conf
    styles to geral style

--- src/app/frame/frame.component.ts
+++ src/app/frame/frame.component.ts
@@ -26,7 +26,7 @@ export class FrameComponent implements OnInit, OnChanges {
 
   }
   ngOnChanges() {
-    if(true){
+    if(this.docOrSentence=="doc"){
       if (this.argumentos) {
         this.textoNormalizado = this.argumentos.TextNormalized;
         console.log(this.argumentos.TextNormalized);
@@ -117,65 +117,76 @@ export class FrameComponent implements OnInit, OnChanges {
         //console.log('date scores');
         //console.log(this.dateScores);
       }
-    } else {/*
+    } else {
 
       this.textoNormalizado = this.argumentos.TextNormalized;
       console.log(this.argumentos.TextNormalized);
       this.keywordScores = this.argumentos.RelevantKWs;
       this.dateScores = this.argumentos.Score;
       console.log(this.dateScores);
-
-      this.texto = this.textoNormalizado.replace(/<d>(.*?)<\/d>/gi, (x) => {
-        let valor = x.replace(/<d>/, '');
-        valor = valor.substring(0, valor.length - 4);
-        // console.log('valor:');
-        // console.log(valor);
-        // console.log('score do valor')
-        // console.log(this.dateScores[valor]);
-        let titulo = this.dateScores[valor.toLowerCase()];
-        let cor = '';
-        if (titulo) {
-          console.log('titulo');
-          console.log(titulo["0"]);
-          titulo = titulo["0"];
-        } else {
-          titulo = 'ta a falhar';
-        }
-
-        if (titulo < 0.3) {
-          cor = 'black';
-        } else{
-          cor = 'red';
-          if (titulo < 0.4) {
-            cor = 'green';
-          }else if (titulo > 0.4) {
-            cor = 'blue';
-            if (titulo > 0.6) {
-              cor = 'yellow';
-            }
-            if (titulo > 0.8) {
-              cor = 'purple';
-            }
-
+      this.texto='';
+      let frases= [];
+      // tslint:disable-next-line: forin
+      for (let i in this.argumentos.SentencesTokens) {
+        frases.push(this.argumentos.SentencesTokens[i.toString()].join(' '));
+        console.log(frases[i]);
+      }
+      // tslint:disable-next-line: whitespace
+      // tslint:disable-next-line: forin
+      for (let fraseIndex in frases) {
+        frases[fraseIndex].replace(/<d>(.*?)<\/d>/gi, (x) => {
+          let valor = x.replace(/<d>/, '');
+          valor = valor.substring(0, valor.length - 4);
+          let titulo = this.dateScores[valor.toLowerCase()];
+          let cor = '';
+          if (titulo) {
+            //console.log('titulo');
+            //console.log(titulo[Object.keys(titulo)[0].toString()][0]);
+            titulo = titulo[Object.keys(titulo)[0].toString()][0];
+          } else {
+            titulo = 'ta a falhar';
           }
-        } let textoAEscrever = '';
-        for (let i=0 ; i < this.argumentos.TempExpressions.length;i++){
-          //console.log('expressoes temporais');
-          let ind = i + '';
-          //console.log(this.argumentos.TempExpressions[ind]);
-          if (this.argumentos.TempExpressions[ind][0].toString() == x.substring(3, x.length - 4)){
+          if (titulo < 0.3) {
+            cor = 'black';
+          } else{
+            cor = 'red';
+            if (titulo < 0.4) {
+              cor = 'green';
+            }else if (titulo > 0.4) {
+              cor = 'blue';
+              if (titulo > 0.6) {
+                cor = 'yellow';
+              }
+              if (titulo > 0.8) {
+                cor = 'purple';
+              }
 
-            textoAEscrever = this.argumentos.TempExpressions[ind][1].toString();
+            }
           }
-      };
+          console.log(cor);
+          let textoAEscrever = '';
+          for (let i = 0 ; i < this.argumentos.TempExpressions.length; i++){
+            //console.log('expressoes temporais');
+            let ind = i + '';
+            //console.log(this.argumentos.TempExpressions[ind]);
+            if (this.argumentos.TempExpressions[ind][0].toString() == x.substring(3, x.length - 4)){
+
+              textoAEscrever = this.argumentos.TempExpressions[ind][1].toString();
+              console.log('tempExp');
+              console.log(this.argumentos.TempExpressions[ind][1].toString());
+            }
+          };
 
         // console.log(titulo);
         // console.log(cor);
         x = '<span title="' + titulo+ '">' + '<b class="'+cor+'">' + textoAEscrever + '</b>' + '</span>';
 
-        return x;
-      });
-        */
+
+          return x;
+        });
+      }
+      this.texto = frases.join(' ');
+
     }
 
   }
