7b203292a993596fcaf18d2b7084d6f0bcdc129b
DarVoid <jorgemduque@hotmail.com>
2020-05-11 06:41:47 +0100

    I DID IT

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+declare var $: any;
+declare var TL: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'timematters';
+export class AppComponent implements OnInit  {
+  ngOnInit(): void {
+
+    /**/
+
+  }
+  title = 'Time Matters';
 }
--- src/app/timeline-scroll/timeline-scroll.component.ts
+++ src/app/timeline-scroll/timeline-scroll.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import TL from '../../assets/TL1.js';
+//import TL from '../../assets/TL1.js';
 declare  var TL: any;
 
 
@@ -21,13 +21,10 @@ export class TimelineScrollComponent implements OnInit {
   constructor() {
     //console.log(this.TLObj);
     this.rendering = 'rendering';
-    this.texto = '';
     this.titulo = 'Timeline';
    }
 
   ngOnInit() {
-    console.log(this.argumentos);
-    this.other();
   }
   public update() {
 
@@ -40,53 +37,19 @@ export class TimelineScrollComponent implements OnInit {
 
       console.log(h);
       console.log(this.argumentos[h]);
-      events.push({start_date:  {year: this.argumentos[h].x}, text: {headline: this.argumentos[h].y.substring(3,this.argumentos[h].y.length-4)}});
+      events.push({"start_date":  {"year": this.argumentos[h].x}, "text": {"headline": this.argumentos[h].y.substring(3,this.argumentos[h].y.length-4)}});
     }
-    j={events: events};
+    j={"events": events};
 
-    j = JSON.stringify(j);
     console.log(j);
-    /*let additionalOptions = {
+    let additionalOptions = {
       start_at_end: true,
       default_bg_color: {r:0, g:0, b:0},
       timenav_height: 250
-    }*/
-    // tslint:disable-next-line: no-unused-expression
-    new TL.Timeline('my-timeline', j);// ,additionalOptions
-
-  }
-  public other() {
-
-    let j: any;
-
-    let events = [];
-    // tslint:disable-next-line: forin
-    for (let h in this.argumentos) {
-
-      console.log(h);
-      console.log(this.argumentos[h]);
-      events.push({start_date:  {year: this.argumentos[h].x}, text: {headline: this.argumentos[h].y.substring(3,this.argumentos[h].y.length-4)}});
     }
-    j={events: events};
+    // tslint:disable-next-line: no-unused-expression
+    new TL.Timeline('my-timeline', j, additionalOptions);
 
-    j = JSON.stringify(j);
-    console.log(j);
-    this.texto = `<link title="timeline-styles" rel="stylesheet" href="https://cdn.knightlab.com/libs/timeline3/latest/css/timeline.css">
-  <script src="https://cdn.knightlab.com/libs/timeline3/latest/js/timeline.js"></script>
-  <div id='timeline-embed' style="width: 100%; height: 200px"></div><script type="text/javascript">
-  var options = {
-    hash_bookmark: false,
-    initial_zoom: 5,
-    script_path: '../../assets/TL1.js'
-  }
-  //window.timeline =
-  var timeline = new TL.Timeline('timeline-embed',
-                                 `+j+`,
-                                 options);
-</script>` ;
-  }
-  public doStuff() {
-    console.log('yay');
   }
 
 }
