3de71d9cc1c499ba4388d2a61b6b5d0fc5e4c9e1
DarVoid <jorgemduque@hotmail.com>
2020-06-11 22:57:07 +0100

    details

--- src/app/score/score.component.ts
+++ src/app/score/score.component.ts
@@ -16,6 +16,7 @@ export class ScoreComponent implements OnChanges {
   public currentlyShowing: string;
   public chartType: string;
   public chartColors: Array<any>;
+  public chartColors2: Array<any>;
   public chartLabels: Array<any>;
   public chartOptions: any;
   public chartDatasets: Array<any>;
@@ -23,7 +24,7 @@ export class ScoreComponent implements OnChanges {
   public chartLabelsKeyword: Array<any>;
   constructor() {
 
-    this.chartType = 'horizontalBar';
+    this.chartType = 'bar';
 
 
     this.chartOptions = {
@@ -51,23 +52,68 @@ export class ScoreComponent implements OnChanges {
 
 
       let dados1 = [];
+      let cores1 = [];
       let dados2 = [];
+      let cores2 = [];
+      //let sortedScores = [this.argumentos.Score].sort((a,b)=>{return a[0]-b[0];});
       // tslint:disable-next-line: forin
       for (let i in this.argumentos.Score) {
         dados2.push(i);
         dados1.push(this.argumentos.Score[i][0]);
+        if(this.argumentos.Score[i][0] < 0.3){
+          cores1.push('rgba(245, 81, 81, 1)');
+          cores2.push('rgba(245, 81, 81, 1)');
+        }else{
+          cores1.push('rgba(103, 172, 219, 1)');
+          cores2.push('rgba(103, 172, 219, 1)');
+        }
       }
+      // tslint:disable-next-line: forin
+      /* for (let i in sortedScores) {
+        dados2.push(i);
+        dados1.push(sortedScores[i][0]);
+        if(sortedScores[i][0] < 0.3){
+          cores1.push('rgba(245, 81, 81, 1)');
+          cores2.push('rgba(245, 81, 81, 1)');
+        }else{
+          cores1.push('rgba(103, 172, 219, 1)');
+          cores2.push('rgba(103, 172, 219, 1)');
+        }
+      }*/
+      this.chartColors = [
+        {
+          backgroundColor: cores1,
+          borderColor: cores2,
+          borderWidth: 2,
+        }
+      ];
       this.chartDatasets = [
         { data: dados1, label: false }
       ];
       this.chartLabels = dados2;
       let dados3 = [];
       let dados4 = [];
+      let cores3 = [];
+      let cores4 = [];
       // tslint:disable-next-line: forin
       for (let i in this.argumentos.RelevantKWs) {
         dados4.push(i);
         dados3.push(this.argumentos.RelevantKWs[i]);
+        if(this.argumentos.RelevantKWs[i]>0.1){
+          cores3.push('rgba(245, 81, 81, 1)');
+          cores4.push('rgba(245, 81, 81, 1)');
+        }else{
+          cores3.push('rgba(103, 172, 219, 1)');
+          cores4.push('rgba(103, 172, 219, 1)');
+        }
       }
+      this.chartColors2 = [
+        {
+          backgroundColor: cores3,
+          borderColor: cores4,
+          borderWidth: 2,
+        }
+      ];
       this.chartDatasets = [
         { data: dados1, label: 'Date Importance' }
       ];
