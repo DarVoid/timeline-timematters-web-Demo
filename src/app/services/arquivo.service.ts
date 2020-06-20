e9f33f1863e6652d4e07aea718717708a0a8bca7
DarVoid <jorgemduque@hotmail.com>
2020-06-20 08:38:41 +0100

    arquivo

---
+++ src/app/services/arquivo.service.ts
@@ -0,0 +1,71 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ArquivoService {
+  url: string;
+  constructor(private http: HttpClient) {
+    this.url = 'https://arquivo.pt/textsearch';
+  }
+
+  public getLinkFromOptions(search: string, options: any): Observable<any> {
+    const formData = new FormData();
+    let realURL = this.url + '?q=' + search;
+    if (options.siteSearch != 'none') {
+      realURL += '&siteSearch=' + options.siteSearch;
+    }
+    if (options.from) {
+      realURL += '&from=' + options.from;
+    }
+    if (options.to) {
+      realURL += '&to=' + options.to;
+    }
+    if (options.type) {
+      realURL += '&type=' + options.type;
+    }
+    if (options.offset) {
+      realURL += '&offset=' + options.offset;
+    }
+    if (options.collection) {
+      realURL += '&collection=' + options.collection;
+    }
+    if (options.maxItems) {
+      realURL += '&maxItems=' + options.maxItems;
+    }
+    if (options.itemsPerSite) {
+      realURL += '&itemsPerSite=' + options.itemsPerSite;
+    }
+    if (options.fields) {
+      realURL += '&fields=' + options.fields[0];
+      for (const k in options.fields) {
+        console.log(k);
+        if (k.toString() != "0") {
+
+          realURL += ',' + options.fields[k];
+        }
+      }
+    }
+    if (options.callback) {
+      realURL += '&callback=' + options.callback;
+    }
+    if (options.prettyPrint) {
+      realURL += '&prettyPrint	=' + options.prettyPrint;
+    }
+
+
+    return this.http.post(realURL, formData
+        ).pipe(map((res, err) => {
+      if (res) {
+        // console.log(res);
+        return res;
+      } else {
+        console.log(err);
+        return err;
+      }
+    }));
+}
+}
