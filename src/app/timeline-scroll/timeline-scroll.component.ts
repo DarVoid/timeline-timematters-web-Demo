af7190fdf77c5092f18bcd10f1cd8da021d2cd1e
DarVoid <jorgemduque@hotmail.com>
2020-09-15 02:54:11 +0100

    Revert "attempt"
    This reverts commit e1007f8d91eeed35347629ea990ceebd3a1acbd2.

--- src/app/timeline-scroll/timeline-scroll.component.ts
+++ src/app/timeline-scroll/timeline-scroll.component.ts
@@ -3,7 +3,6 @@ import {MatSnackBar} from '@angular/material/snack-bar';
 import { YakeService } from '../services/yake.service';
 import { ArquivoService } from '../services/arquivo.service';
 import { take } from 'rxjs/operators';
-import { forkJoin } from 'rxjs';
 // import TL from '../../assets/TL1.js';
 declare  var TL: any;
 declare  var $: any;
@@ -20,10 +19,12 @@ export class TimelineScrollComponent implements OnInit {
   @Input() docSen: any;
   @Input() argumentosRelevantes: any;
   public relevant: boolean;
-  public relevant2: boolean;
   public argumentos: any;
+  public obejct: any;
+  public rendering: string;
   public texto: string;
   public events: Array<any>;
+  public titulo: string;
   public timeline: any;
   public isSet: boolean;
   public jsonText: string;
@@ -32,12 +33,13 @@ export class TimelineScrollComponent implements OnInit {
   public relevant_string:string;
   constructor(private _snackBar: MatSnackBar, private yake:YakeService, private arquivo:ArquivoService) {
     // console.log(this.TLObj);
+    this.rendering = 'rendering';
+    this.titulo = 'Timeline';
     this.isSet = false;
     this.isnotset = true;
     this.loading = false;
-    this.relevant = false;
-    this.relevant2 = false;
-    this.relevant_string = "Showing all dates";
+    this.relevant = true;
+    this.relevant_string = "Showing relevant dates only";
    }
 
   ngOnInit() {
@@ -47,26 +49,26 @@ export class TimelineScrollComponent implements OnInit {
     
     if(document.getElementById("my-timeline") && this.isnotset){
       this.isnotset = false;
-      this.argumentos = this.argumentosTodos;
-      this.loading = true;
-      this.update();
-      this.isSet = true;
+      this.setRelevance()
     }
 
   }
   setRelevance() {
+    if (!this.isSet) {
+      this.isSet = true;
+    }
     if (this.relevant) {
-      this.argumentos = this.argumentosTodos;
-      this.relevant_string = "Showing all dates";
+      this.argumentos = this.argumentosRelevantes;
+      this.relevant_string = "Showing relevant dates only";
       this.relevant = false;
 
     } else {
-      this.argumentos = this.argumentosRelevantes;
-      this.relevant_string = "Showing relevant dates only";
+      this.argumentos = this.argumentosTodos;
+      this.relevant_string = "Showing all dates";
       this.relevant = true;
 
     }
-    this.loading = true;
+    this.loading= true;
     this.update();
   }
   public copyToClipboard(event: any) {
@@ -85,6 +87,11 @@ export class TimelineScrollComponent implements OnInit {
   }
 
   update() {
+    this.rendering = '';
+    console.log('argumentosTodos');
+    console.log(this.argumentosTodos);
+    console.log('argumentosRel');
+    console.log(this.argumentosRelevantes);
 
     let j: any;
 
@@ -93,17 +100,17 @@ export class TimelineScrollComponent implements OnInit {
     for (let h=0; h<this.argumentos.length; h++) {
       console.log("conteudo");
       console.log(this.argumentos[h].y.split("</p>")[1].split("(...)").join("").split("<kw>").join("").split("</kw>").join("").split("<d>").join("").split("</d>").join(""));
-      
+      let captio;
       this.yake.getKeywords(this.argumentos[h].y.split("</p>")[1].split("(...)").join("").split("<kw>").join("").split("</kw>").join("").split("<d>").join("").split("</d>").join("")).pipe(take(1)).subscribe((res)=>{
         if (res) {
           console.log(res);
-          let captio=res.keywords[0].ngram;
+          captio=res.keywords[0].ngram;
           this.arquivo.getImgURL(captio).pipe(take(1)).subscribe((res2:any)=>{
             if(res2){
               console.log(res2);
               let url2=res2.responseItems[0].imgLinkToArchive;
-              console.log("Storyline rule-based");
-              if (this.argumentos[h].x.length == 4) {
+                  // console.log(this.argumentos[h]);
+              if (this.argumentos[h].x.length === 4) {
                 events.push({ start_date:  { year: this.argumentos[h].x},media:{thumbnail: url2, url:url2,link: url2, credit: '<a class="preto" target="_blank" href="https://www.arquivo.pt"><p>powered by Arquivo.pt</p></a>'}, text: { headline : '<p class="changeCaptio">'+captio.substring(0,1).toUpperCase()+captio.substring(1,captio.length)+'</p>', text: this.argumentos[h].y}}); //
         
               } else if (this.argumentos[h].x.split('-').length === 2) {
@@ -114,26 +121,22 @@ export class TimelineScrollComponent implements OnInit {
                 // tslint:disable-next-line: max-line-length
                 events.push({start_date:  {year: this.argumentos[h].x.substring(0,10).split('-')[0],month: this.argumentos[h].x.substring(0,10).split('-')[1], day: this.argumentos[h].x.substring(0,10).split('-')[2] },media:{thumbnail: url2,url:url2,link: url2, credit: '<a class="preto" target="_blank"   href="https://www.arquivo.pt"><p>powered by Arquivo.pt</p></a>'}, text: { headline : "<p>"+captio.substring(0,1).toUpperCase()+captio.substring(1,captio.length)+"</p>", text: this.argumentos[h].y}}); 
               }
-
               if(h == this.argumentos.length-1){
+                j = {events: events};
+                this.jsonText = j;
+                console.log(j);
+                const additionalOptions = {
+                  start_at_end: false,
+                  timenav_height: 10,
+                  default_bg_color: {r: 255, g: 255, b: 255},
+                  trackResize: "false"
+                };
+                // tslint:disable-next-line: no-unused-expression
+                new TL.Timeline('my-timeline', j, additionalOptions);
                 this.loading  = false;
-                let a = setTimeout(function(){ 
-                  j = {events: events};
-                  this.jsonText = j;
-                  console.log(j);
-                  const additionalOptions = {
-                    start_at_end: false,
-                    timenav_height: 10,
-                    default_bg_color: {r: 255, g: 255, b: 255},
-                    trackResize: "false"
-                  };
-                  
-                  // tslint:disable-next-line: no-unused-expression
-                  new TL.Timeline('my-timeline', j, additionalOptions);
-                }, 1500); //tentar por isto com um w8 for all
-
-               
               }
+
+
             }
           });
           
