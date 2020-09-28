ae5fa75d9ae26ff1d4c53924ba21d4af6a03c392
DarVoid <jorgemduque@hotmail.com>
2020-09-28 23:38:09 +0100

    not sure about this

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import { KeywordExctractionComponent } from './keyword-exctraction/keyword-exctraction.component';
 
 export const routes: Routes = [
   {
-      path: ''
+      path: '',
+      component: HomeComponent
   }
 ];
