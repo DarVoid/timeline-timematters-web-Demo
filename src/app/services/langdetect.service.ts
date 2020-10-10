c42a26162fc379820c3b76e3385943be58d4f00a
DarVoid <jorgemduque@hotmail.com>
2020-10-10 18:10:57 +0100

    no more shenanigans

--- src/app/services/langdetect.service.ts
+++ src/app/services/langdetect.service.ts
@@ -9,13 +9,15 @@ import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http
 export class LangdetectService {
   url: string;
   constructor(private http: HttpClient) {
-    this.url = 'https://langdec-api-heroku.herokuapp.com/langdetect/'; //'https://tm-websuiteapps.ipt.pt/languageDetection/api/v1.0';
+    //this.url = 'https://langdec-api-heroku.herokuapp.com/langdetect/';
+     this.url = '/languageDetection/api/v1.0';
   }
 
 
   public getLanguageFromContent(search: string): Observable<any> {
     const formData = new FormData();
-    formData.append('text', search);
+    //formData.append('text', search);
+    formData.append('content', search);
     return this.http.post(this.url, formData,
         ).pipe(map((res, err) => {
       if (res) {
